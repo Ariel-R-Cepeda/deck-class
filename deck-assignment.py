62530583221c918d8ec608ebcba1ecaa81353b40
Ariel-R-Cepeda <43122482+Ariel-R-Cepeda@users.noreply.github.com>
2018-11-19 21:37:56 -0500

    Put stuff in the thing
    -Add new class: Deck
    -Attempt to add new class: Card
    -Add new method: deal
    -remove neat format

--- deck-assignment.py
+++ deck-assignment.py
@@ -1,22 +1,45 @@
 # Import statements go here
+ import random
+ import collections
+
+card= collections.namedtuple('card',['rank','suit'])
+#idk what to do with this information cuz almost all of my code has been written already
+
+  ranks= ["2","3","4","5","6","7","8","9","10","Q","K","A","J"]
+  suits= ["clubs","diamonds","hearts","spades"]
+  all_cards= [None]
+  r=0
+  s=0
+  counts=0
+  for idfk in range(52):
+     all_cards.extend(str(ranks[r] + suits[s]))
+     r+=1
+     counts+=1
+     if counts == 13:
+        r=0
+        s+=1
+        #AHH its the index is out of range and idk how to fix this, cuz I dont get why its out of range
+        #ps, I coded this before reading the format you wanted this in, whoops, so much for that format
+def __init__(self, ranks, suits):
+    if self.ranks not in ranks:
+      return ValueError
+    if self.suits not in suits:
+      return ValueError
 
 
-class Deck:
-    """
-    Your docstring goes here
-    """
-
-    ranks = [str(r) for r in range(2, 11)] + list('JQKA')
-    suits = list('cdhs')
-
-    def __init__(self):
-        self._cards = []  # Your code goes inside the listcomp
-
-    def __repr__(self):
-        pass
+  def __repr__(self):
+    return all_cards
+  def deal(self):
+    choice_suit_ind=random.randint(0,12)
+    choice_rank_ind=random.randint(0,3)
+    choice_suit= suits[choice_suit_ind]
+    choice_rank= ranks[choice_rank_ind]
+    all_cards_ind= (choice_suit_ind + 1)*(choice_rank_ind + 1)-1
+    del all_cards[all_cards_ind]
+    return f'{choice_rank} of {choice_suit}'
+    
 
     def __len__(self):
-        return len(self._cards)
+        return len(all_cards)
 
-    def deal(self):
-        pass
+ 
